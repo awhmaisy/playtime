6a81a3441d2216d9fa12407ada8034498586f3b7
mei higashi <mei@meihigashi.com>
2024-07-20 14:09:13 -0700

    new schtuff

--- src/routes/adopt me/+page.svelte
+++ src/routes/adopt me/+page.svelte
@@ -4,3 +4,24 @@
 	the now.
 </p>
 
+<div class="photo-grid">
+	<a href="https://www.ebay.com/itm/285970661257?itmmeta=01J38YPASJYDFAP2G8V46QVZ7Y&hash=item42952dff89:g:oWIAAOSwyVRmnBwW&itmprp=enc:AQAJAAAA4Jj0OU6sYtTrPpeIjYmmcUJyXK+YF+KPQs2YvhoeCRAVkJkMrT5zn+5F4lPQyabIKgAnry/Ls0FpCJWyXSCGzXT1vkkVyEf8tJjHqDPnV2xkV1Glhkvc4ronxPAgrM/BSAtLsMvDGWY+f/Y7yhe4x8FfbiJRSynJkuPCO86UhiogMIErdCYkHt0vGr5WGLp9Me2dClqJJ+JeBgfuScI8P1iqlx4zWrqt+56xJzHyGdVTqd9k5At7MQ0RUHxeF0yxseRQOMTqYR20CTNwVS2OgTrH8Cg9jaiJm9R0moD2swM8%7Ctkp:Bk9SR-6s2Z6aZA" target="_blank">
+		<img src="/images/dgpill.jpg" alt="dgpill" />
+	</a>
+	<a href="https://shop.year0001.com/na/products/bladee-razorwire-t-shirt-black" target="_blank">
+		<img src="/images/razorwire.jpg" alt="bladee" />
+	</a>
+</div>
+
+<style>
+	.photo-grid {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		gap: 10px; /* Adjust the gap as needed */
+	}
+	.photo-grid img {
+		width: 100%;
+		height: auto;
+		object-fit: cover; /* Ensures images cover the grid cell */
+	}
+</style>
